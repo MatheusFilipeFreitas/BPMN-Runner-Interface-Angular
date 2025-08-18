34330de71816d4565e712ba6e1636f1a95f4d968
Matheus Freitas <mathffreitas@Matheuss-Laptop.local>
2025-08-18 18:59:56 -0300

    Adjusting infinity reload in page

--- src/app/app.router-provider.ts
+++ src/app/app.router-provider.ts
@@ -1,103 +1,93 @@
- import { DOCUMENT, EnvironmentProviders, inject, provideEnvironmentInitializer } from "@angular/core";
-import {
-  createUrlTreeFromSnapshot,
-  NavigationCancel,
-  NavigationError,
-  NavigationStart,
-  provideRouter,
-  RedirectCommand,
-  Router,
-  withComponentInputBinding,
-  withInMemoryScrolling,
-  withNavigationErrorHandler,
-  withRouterConfig,
-  withViewTransitions,
-} from "@angular/router";
-import { map, merge, Subject, fromEvent } from "rxjs";
+import { DOCUMENT, inject, provideEnvironmentInitializer } from "@angular/core";
+import { createUrlTreeFromSnapshot, NavigationCancel, NavigationError, NavigationStart, provideRouter, RedirectCommand, Router, withComponentInputBinding, withInMemoryScrolling, withNavigationErrorHandler, withRouterConfig, withViewTransitions } from "@angular/router";
+import { map, merge, Subject } from "rxjs";
 import { WINDOW } from "./utils/window";
 import { AppScroller } from "./app.scroller";
 import { routes } from "./app.routes";
 import { HttpErrorResponse } from "@angular/common/http";
 
 const transitionCreated = new Subject<void>();
-
-export const routerProviders: EnvironmentProviders[] = [
+export const routerProviders = [
   provideRouter(
     routes,
     withInMemoryScrolling(),
-    withRouterConfig({ canceledNavigationResolution: "computed" }),
-    withNavigationErrorHandler(({ error }) => {
-      const router = inject(Router);
+    withRouterConfig({canceledNavigationResolution: 'computed'}),
+    withNavigationErrorHandler(({error}) => {
       if (error instanceof HttpErrorResponse) {
-        return new RedirectCommand(router.parseUrl("/404"));
+        return new RedirectCommand(inject(Router).parseUrl('/404'));
       }
       return void 0;
     }),
     withViewTransitions({
-      onViewTransitionCreated: ({ transition, to }) => {
+      onViewTransitionCreated: ({transition, to}) => {
         transitionCreated.next();
         const router = inject(Router);
         const toTree = createUrlTreeFromSnapshot(to, []);
         if (
           router.isActive(toTree, {
-            paths: "exact",
-            matrixParams: "exact",
-            fragment: "ignored",
-            queryParams: "ignored",
+            paths: 'exact',
+            matrixParams: 'exact',
+            fragment: 'ignored',
+            queryParams: 'ignored',
           })
         ) {
           transition.skipTransition();
         }
       },
     }),
-    withComponentInputBinding()
+    withComponentInputBinding(),
   ),
   provideEnvironmentInitializer(() => inject(AppScroller)),
-  provideEnvironmentInitializer(() => setupNavigationAdapter()),
+  provideEnvironmentInitializer(() => initializeNavigationAdapter()),
 ];
 
-const setupNavigationAdapter = (): void => {
+const initializeNavigationAdapter = () => {
   const router = inject(Router);
   const window = inject(WINDOW);
-  const document = inject(DOCUMENT);
   const navigation = (window as any).navigation;
-
-  if (!navigation || !document.startViewTransition) {
+  if (!navigation || !inject(DOCUMENT).startViewTransition) {
     return;
   }
 
-  let cancelPendingNavigation: (() => void) | undefined;
-
-  const navigateError$ = fromEvent(navigation, "navigateerror");
-  const navigate$ = fromEvent(navigation, "navigate");
-
-  navigateError$.subscribe(() => {
-    cancelPendingNavigation?.();
-    cancelPendingNavigation = undefined;
+  let intercept = false;
+  let clearNavigation: (() => void) | undefined;
+  navigation.addEventListener('navigateerror', async () => {
+    if (!clearNavigation) {
+      return;
+    }
+    clearNavigation = undefined;
     router.getCurrentNavigation()?.abort();
   });
-
-  navigate$.subscribe((navigateEvent: any) => {
+  navigation.addEventListener('navigate', (navigateEvent: any) => {
+    if (!intercept) {
+      return;
+    }
     navigateEvent.intercept({
       handler: () =>
         new Promise<void>((_, reject) => {
-          cancelPendingNavigation = () => {
-            cancelPendingNavigation = undefined;
+          clearNavigation = () => {
+            clearNavigation = undefined;
             reject();
           };
         }),
     });
   });
 
-  merge(transitionCreated.pipe(map(() => "viewtransition")), router.events).subscribe((e) => {
+  merge(transitionCreated.pipe(map(() => 'viewtransition')), router.events).subscribe((e) => {
     const currentNavigation = router.getCurrentNavigation();
-    if (currentNavigation?.trigger === "popstate" || currentNavigation?.extras.replaceUrl) {
+    if (currentNavigation?.trigger === 'popstate' || currentNavigation?.extras.replaceUrl) {
       return;
     }
     if (e instanceof NavigationStart) {
-      window.history.replaceState(window.history.state, "", window.location.href);
-    } else if (e === "viewtransition" || e instanceof NavigationCancel || e instanceof NavigationError) {
-      cancelPendingNavigation?.();
+      intercept = true;
+      window.history.replaceState(window.history.state, '', window.location.href);
+      intercept = false;
+    } else if (
+      e === 'viewtransition' ||
+      e instanceof NavigationCancel ||
+      e instanceof NavigationError
+    ) {
+      clearNavigation?.();
     }
   });
 };
