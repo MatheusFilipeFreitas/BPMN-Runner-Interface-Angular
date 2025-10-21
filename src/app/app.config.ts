717246360851b4c21fb8ec9750a16e13758ac420
Matheus Filipe Nascimento de Freitas <mathffreitas@MacBook-Air-de-Matheus.local>
2025-10-21 08:29:08 -0300

    getting env configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,14 @@ import { ApplicationConfig, EnvironmentProviders, ErrorHandler, InjectionToken, 
 import { routerProviders } from './app.router-provider';
 import { WINDOW, windowProvider } from './utils/window';
 import { LOCAL_STORAGE, localStorageProvider } from './utils/local-storage';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environments';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { provideHttpClient } from '@angular/common/http';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 type ErrorProvider = {
   provide: typeof ErrorHandler;
@@ -45,6 +53,23 @@ export const errorProviders: (EnvironmentProviders | ErrorProvider)[] = [
   { provide: ErrorHandler, useClass: GlobalErrorHandler }
 ];
 
+export const firestoreProviders: (EnvironmentProviders)[] = [
+  provideFirebaseApp(() => initializeApp(environment.firebase)),
+  provideAuth(() => getAuth())
+];
+
+export const primengProviders: (EnvironmentProviders)[] = [
+  provideAnimationsAsync(),
+  providePrimeNG({
+    theme: {
+      preset: Aura
+    }
+  })
+];
+
+export const httpClientProviders: (EnvironmentProviders)[] = [
+  provideHttpClient()
+];
 
 export const appConfig: ApplicationConfig = {
   providers: [
