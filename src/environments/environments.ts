a27c034989b83422914e66932bb336e306fffb99
Matheus Filipe Nascimento de Freitas <mathffreitas@MacBook-Air-de-Matheus.local>
2025-10-26 21:46:31 -0300

    Merge banch 'bpr15' into 'main'

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,13 +2,13 @@ export const environment = {
     production: false,
     API_PATH: "http://localhost:8080/api",
     firebase: {
-        apiKey: 'AIzaSyCmuauf0zhwxa0PRWofshz20nDiUTqaLnM',
-        authDomain: 'bpmn-runner.firebaseapp.com',
-        projectId: 'bpmn-runner',
-        storageBucket: 'bpmn-runner.firebasestorage.app',
-        messagingSenderId: '795159592229',
-        appId: '1:795159592229:web:4d09443c8ab36a941baecc',
-        measurementId: 'G-LNTMRE3194',
+        apiKey: 'FIREBASE_API_KEY',
+        authDomain: 'FIREBASE_AUTH_DOMAIN',
+        projectId: 'FIREBASE_PROJECT_ID',
+        storageBucket: 'FIREBASE_STORAGE_BUCKET',
+        messagingSenderId: 'FIREBASE_MESSAGING_SENDER_ID',
+        appId: 'FIREBASE_APP_ID',
+        measurementId: 'FIREBASE_MEASUREMENT_ID'
     },
     dev_api_key: null
 }
